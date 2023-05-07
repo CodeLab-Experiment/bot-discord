1540cc79eea99b1a160d5b2d78ccfcb779d2dda7
Le Khac Thanh Tung <lekhacthanhtung.it@gmail.com>
2023-05-07 16:10:05 +0700

    feat: get avatar of user & create the same msg with the one user enters

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
+const { Client, GatewayIntentBits, ActivityType, EmbedBuilder, Embed } = require("discord.js");
 const client = new Client({
 	intents: [
 		GatewayIntentBits.Guilds,
@@ -25,11 +25,38 @@ client.on("ready", () => {
 })
 
 client.on("messageCreate", (message) => {
-	console.log(message.content)
-	if (message.content.toLowerCase() == 'ping') {
-		message.reply('pong')
-		.then(() => console.log(`Replied to message "${message.content}"`))
-		.catch(console.error);;
+	if (message.author.bot) return;
+	const prefix = "/";
+	if (!message.content.startsWith(prefix)) return; // not a slash command
+	const args = message.content.slice(prefix.length).split(" ");
+	const command = args.shift().toLowerCase()
+	switch (command) {
+		case 'ping':
+			message.reply(`🏓pong \`${client.ws.ping}ms\``)
+			break;
+		case 'say':
+			if (message.deletable) message.delete();
+			message.channel.send(args.join(' '))
+			break;
+		case 'avatar':
+			// @ somebody in Discord => mention (tag)
+			// guild -> server
+			const member = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.member // member who sends the msg
+			const avatarURL = member.displayAvatarURL({
+				extension: "png",
+			})
+			console.log(avatarURL)
+			const name = member.displayName || member.username;
+			const embed = new EmbedBuilder()
+				.setTitle(`This is ${name}'s avatar`)
+				.setImage(avatarURL)
+			message.channel.send({
+				embeds: [embed]
+			})
+			break;
+		default:
+			message.reply('Invalid command')
+			break;
 	}
 })
 
